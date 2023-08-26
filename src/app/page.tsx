f49bcb57fe06bab57a7ba14c68a744cf78b383ff
tmoran18 <support@tim-moran.com>
2023-08-26 14:10:07 +1000

    check if it's the data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,5 @@ import supabase from "@/lib/db";
 export default async function Home() {
   const exercises = await supabase.from("exercise_library").select("*");
 
-  return (
-    <div className="container px-6">
-      <Button className="font-sans uppercase w-full text-xl p-6 border-[2.5px] border-gray-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-2xl tracking-wider bg-orange-600 bg-[url('../../public/diamonds.svg')] bg-repeat text-white my-5 ">
-        TESTING THIS BUTTON
-      </Button>
-      {JSON.stringify(exercises)}
-    </div>
-  );
+  return <div className="container px-6 border">Hi</div>;
 }
