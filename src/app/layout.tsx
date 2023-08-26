50513582c883bfe22ffbfabfcbe0dd39e5b1dfaf
tmoran18 <support@tim-moran.com>
2023-08-26 14:15:47 +1000

    Try fix mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,17 +14,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-      />
       <meta name="description" content="Description" />
       <meta name="keywords" content="Keywords" />
       <title>Workout</title>
 
-      <link rel="manifest" href="../../public/manifest.json" />
-      <link href="../../public/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-      <link href="../../public/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+      <link rel="manifest" href="/manifest.json" />
+      <link href="/public/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
+      <link href="/public/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
       <meta name="theme-color" content="#24272C" />
       <body className={inter.className} style={{ background: "black" }}>
         {children}
