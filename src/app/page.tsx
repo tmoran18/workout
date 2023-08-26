64a91a5ad1e8ab8fb30ff2a55c23126e1c6b5103
tmoran18 <support@tim-moran.com>
2023-08-26 14:02:21 +1000

    test viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,11 @@ export default async function Home() {
   const exercises = await supabase.from("exercise_library").select("*");
 
   return (
-    <main className="text-white">
-      <pre>{JSON.stringify(exercises, null, 2)}</pre>
-      <pre>
-        <div className="container px-6">
-          <Button className="font-sans uppercase w-full text-xl p-6 border-[2.5px] border-gray-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-2xl tracking-wider bg-orange-600 bg-[url('../../public/diamonds.svg')] bg-repeat text-white my-5 ">
-            TESTING THIS BUTTON
-          </Button>
-        </div>
-      </pre>
-    </main>
+    <div className="container px-6">
+      <Button className="font-sans uppercase w-full text-xl p-6 border-[2.5px] border-gray-400 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] text-2xl tracking-wider bg-orange-600 bg-[url('../../public/diamonds.svg')] bg-repeat text-white my-5 ">
+        TESTING THIS BUTTON
+      </Button>
+      {JSON.stringify(exercises)}
+    </div>
   );
 }
